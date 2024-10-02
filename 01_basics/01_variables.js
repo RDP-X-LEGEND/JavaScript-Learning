7c860c748f8dc1ef52b8e296f69557976eddacb3
Rajdeep Pal <121654824+Rajdeep-Works@users.noreply.github.com>
2024-10-02 19:14:00 +0000

    datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12789
+let accountEmail = "rdp12100@gmail.com"
+var accountPassword = "12100"
+accountCity = "NoWhere"
+let accountState 
+
+// accountId = 24 // not allowed
+
+accountEmail = "rdp100@gmail.com"
+accountPassword = "100"
+accountCity = "NowHere"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Var ta use korbo na ota bage... Ota te problem hoye
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // eta likle js r new version ta use hobe puro program a , at pore change kora jabe na
+
+// alert( 3 + 2 )  // eta browser a hoye kintu node a te hoye na
+
+console.log( 3 + 3 ) // amon code likte hobe jeta pora jabe pore
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
