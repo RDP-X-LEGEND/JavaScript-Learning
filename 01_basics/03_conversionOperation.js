6b9c87aa7b10d1709b5266a467dc9011f88ca83a
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-03 12:52:44 +0000

    conversion of datatypes done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "100"
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLogged = true
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 1234
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
