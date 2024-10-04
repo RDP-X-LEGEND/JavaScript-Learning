63fa258aa1909f1ee858a587b2ed827b5cebe102
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-04 17:10:22 +0000

    video 7 done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "100"
+ let score = "100"
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
@@ -21,3 +21,31 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
 
+
+let value = 12
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Legend"
+let str2 = "RDP"
+let str3 = str1 + " " + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "2" + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
