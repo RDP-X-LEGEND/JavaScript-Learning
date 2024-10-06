66d8b03875264a9d82ebab6232b6a3bf233d2d79
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-06 17:38:46 +0000

    modified output ( all include )

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,25 +6,25 @@ console.log(`Hello my name is ${name} and I has ${repository} repository.`);
 
 let newString = new String("RDP Loves Js")
 
-// console.log(newString.length);
-// console.log(newString.charAt(5));
-// console.log(newString.toUpperCase());
-// console.log(newString.indexOf("L"));
-// console.log(newString.substring(0,3));
-// console.log(newString.slice(4,9));
+console.log(newString.length);
+console.log(newString.charAt(5));
+console.log(newString.toUpperCase());
+console.log(newString.indexOf("L"));
+console.log(newString.substring(0,3));
+console.log(newString.slice(4,9));
 
 let badEntry = "               Hello..           "
 let goodEntry = badEntry.trim()
-// console.log(badEntry);
-// console.log(goodEntry);
+console.log(badEntry);
+console.log(goodEntry);
 
 let wrongUrl = "https://rdp.web/about us"
 let rightUrl = wrongUrl.replace(" ", "%20")
-// console.log(wrongUrl);
-// console.log(rightUrl);
+console.log(wrongUrl);
+console.log(rightUrl);
 
 let includeCheck = "I love my money...."
-// console.log(includeCheck.includes("money"));
+console.log(includeCheck.includes("money"));
 
 let newString2Array = newString.split(" ")
 console.log(newString2Array);
