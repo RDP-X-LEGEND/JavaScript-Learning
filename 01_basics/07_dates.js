c4a40551b6a9a2c129bd7990d6975aafa6c713e9
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-07 14:31:49 +0000

    dates half done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,29 @@
+let newDate = new Date()
+
+// console.log(newDate); // various function of dates 
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toISOString());
+// console.log(newDate.toJSON());
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toLocaleTimeString());
+
+// console.log(typeof newDate); // type of date
+
+let personalDate = new Date(2024, 9, 7) // to add just date
+// console.log(personalDate.toDateString());
+
+let personalDate2 = new Date(2024, 9, 7, 19, 43) // to add time along with date
+// console.log(personalDate2.toLocaleString());
+
+let personalDate3 = new Date("2024-10-7") // to add in american format [inp => (yyyy, mm, dd)] [out => (mm, dd, yyyy)]
+// console.log(personalDate3.toLocaleDateString());
+
+let personalDate4 = new Date("7-10-2024") // to add in indian format [(inp = out) => (dd, mm, yyyy)]
+// console.log(personalDate4.toLocaleDateString());
+
+console.log(personalDate4.getTime()); // convert a given date to milesecond from the 1 january 1970
+
+let nowTime = Date.now() // to add the current time
+console.log(nowTime);
