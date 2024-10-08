4e2dab94c242c54d5193f4a10b8b8605cc330311
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-08 13:16:17 +0000

    arrays half done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+let arr1 = [23, 54, 665, 98473, "Loki", "Thor", "Doc. Strange"]
+
+let arr2 = new Array(0, 2, 6, 5) // declearing array with new method
+
+arr1.push("Doom") // add element in the last of an array
+
+arr2.pop() // remove the last element of an array
+
+arr1.unshift("Please Don't use") // add element in the last of an array // BUT NO USE AS IT CONSUMES TIME 
+
+
+
+
+console.log(arr1); 
+console.log(arr2); 
