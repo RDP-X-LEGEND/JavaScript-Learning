f4b7c5fef2ed394163b9a1152a76746fd37ce472
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-09 15:18:43 +0000

    nothing much done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,9 +6,11 @@ arr1.push("Doom") // add element in the last of an array
 
 arr2.pop() // remove the last element of an array
 
-arr1.unshift("Please Don't use") // add element in the last of an array // BUT NO USE AS IT CONSUMES TIME 
+arr1.unshift("Please Don't use") // add element in the first of an array // BUT NO USE AS IT CONSUMES TIME 
 
+arr1.shift() // remove the first element of an array
 
+console.log(arr1.includes);
 
 
 console.log(arr1); 
