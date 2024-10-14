6b5c920d798064b57dc0a1737f0faaf0041ff3ba
Rajdeep Pal <121654824+RDP-X-LEGEND@users.noreply.github.com>
2024-10-14 12:32:16 +0000

    array_1 little change

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,21 +2,26 @@ let arr1 = [23, 54, 665, 98473, "Loki", "Thor", "Doc. Strange"]
 
 let arr2 = new Array(0, 2, 6, 5) // declearing array with new method
 
-// arr1.push("Doom") // add element in the last of an array
+arr1.push("Doom") // add element in the last of an array
 
-// arr2.pop() // remove the last element of an array
+arr2.pop() // remove the last element of an array
 
-// arr1.unshift("Please Don't use") // add element in the first of an array // BUT NO USE AS IT CONSUMES TIME 
+arr1.unshift("Please Don't use") // add element in the first of an array // BUT NO USE AS IT CONSUMES TIME 
 
-// arr1.shift() // remove the first element of an array
+arr1.shift() // remove the first element of an array
 
-// console.log(arr1.includes(52)); // find if it is there or not
+console.log(arr1.includes(52)); // find if it is there or not
 
-// console.log(arr2.indexOf(0)); // finding the index of any vale present
+console.log(arr2.indexOf(0)); // finding the index of any vale present
 
-// console.log(arr2.indexOf(95)); // finding the index of any vale not present
+console.log(arr2.indexOf(95)); // finding the index of any vale not present
+
+console.log(arr1); 
+console.log(arr2); 
 
 let arr3 = arr2.join() // converts the old array to a string and put it in the new
+
+console.log(arr3);
 
 //slice and splice
 
@@ -35,8 +40,3 @@ let arr6 = arr4.splice(1, 3)
 console.log(arr6);
 
 console.log("3", arr4); // checking the result of arr4 after splice
-
-
-// console.log(arr1); 
-// console.log(arr2); 
-// console.log(arr3);
